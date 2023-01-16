851159ad44af1d94caa5fffc1a517aa74d530879
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-16 20:03:13 +0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Игорь Солодянкин on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
