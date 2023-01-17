776ac82a44e983ed37a4ad826a634f6a54ea12b9
Manohin <111537419+Manohin@users.noreply.github.com>
2023-01-17 19:15:55 +0300

    Test commit

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -24,11 +24,13 @@ enum Theme: String {
     case tan
     case teal
     case yellow
+    case red
     
     var accentColor: Color {
         switch self {
         case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
-        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        case .indigo, .magenta, .navy, .oxblood, .purple, .red: return .white
+        
         }
     }
     var mainColor: Color {
